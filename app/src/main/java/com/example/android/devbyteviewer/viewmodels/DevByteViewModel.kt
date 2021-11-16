caf9b78539b96f0f2357137cf50f89b581c0f3e8
Alessandro <alessandro.mana@gmail.com>
2021-11-16 07:18:17 +0100

    Refactored to use LiveData. In Room.kt, updated getVideos() to return a LiveData of a List of DatabaseVideo. Corrected some code according to exercise git diff solution.

--- app/src/main/java/com/example/android/devbyteviewer/viewmodels/DevByteViewModel.kt
+++ app/src/main/java/com/example/android/devbyteviewer/viewmodels/DevByteViewModel.kt
@@ -28,16 +28,6 @@ import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import java.io.IOException
 
-/**
- * DevByteViewModel designed to store and manage UI-related data in a lifecycle conscious way. This
- * allows data to survive configuration changes such as screen rotations. In addition, background
- * work such as fetching network results can continue through configuration changes and deliver
- * results after the new Fragment or Activity is available.
- *
- * @param application The application that this viewmodel is attached to, it's safe to hold a
- * reference to applications across rotation since Application is never recreated during actiivty
- * or fragment lifecycle events.
- */
 class DevByteViewModel(application: Application) : AndroidViewModel(application) {
 
     /**
